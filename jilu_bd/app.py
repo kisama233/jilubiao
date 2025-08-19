4f86401863c808448c86eb3b700c9235bd48b44d
kisama233 <140413586+kisama233@users.noreply.github.com>
2025-08-19 16:52:05 +0800

    Update app.py

--- jilu_bd/app.py
+++ jilu_bd/app.py
@@ -18,7 +18,7 @@ DOCUMENTS_DIR = os.path.join(BASE_DIR, 'documents')
 DB_CONFIG = {
     'host': os.getenv('DB_HOST', 'localhost'),
     'user': os.getenv('DB_USER', 'root'),
-    'password': os.getenv('DB_PASSWORD', 'ki233'),
+    'password': os.getenv('DB_PASSWORD', '*****'),
     'database': os.getenv('DB_NAME', 'jilu_db'),
     'cursorclass': pymysql.cursors.DictCursor
 }
